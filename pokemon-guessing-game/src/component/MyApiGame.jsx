fadf4eac38c2496efec3107c33e2dccb381e82f5
Priti Saxena <saxenapriti1008@gmail.com>
2025-11-27 14:20:58 +0100

    updated MyApiGame.jsx
    added sound effects
    created fetch random pokemon

--- pokemon-guessing-game/src/component/MyApiGame.jsx
+++ pokemon-guessing-game/src/component/MyApiGame.jsx
@@ -1,2 +1,33 @@
 import { useEffect, useState } from "react";
-import "./MyApiGame.css";
+
+
+  function MyApiGame() {
+  const [pokemon, setPokemon] = useState(null);
+  const [guess, setGuess] = useState("");
+  const [result, setResult] = useState("");
+  const [revealed, setRevealed] = useState(false);
+  const [score, setScore] = useState(0);
+
+  // Sound effects
+
+  const correctSound = new Audio("/sounds/correct.wav");
+  const wrongSound = new Audio("/sounds/wrong.wav");
+  const revealSound = new Audio("/sounds/reveal.wav");
+
+// Fetch random Pokemon
+
+  function fetchRandomPokemon() {
+    const randomId = Math.floor(Math.random() * 151) + 1; 
+    fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`)
+      .then(res => res.json())
+      .then(data => {
+        setPokemon(data);
+        setGuess("");
+        setResult("");
+        setRevealed(false);
+      });
+  }
+
+  useEffect(() => {
+    fetchRandomPokemon();
+  }, []) }
